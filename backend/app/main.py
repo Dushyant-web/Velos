a4fc26f67866ac265046296edbce5746cc993cca
dushyant <dushyant@Dushyants-MacBook-Air.local>
2026-02-28 01:42:35 +0530

    Add Netlify to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,15 +6,13 @@ from .routes import router
 
 app = FastAPI()
 
-allow_origins=[
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "https://velosv2.netlify.app/"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow frontend
+    allow_origins=[    
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://velosv2.netlify.app/"],  # allow frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
