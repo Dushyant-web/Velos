570afc808fb85f150f4bdc4a724cca2a9b7358cc
dushyant <dushyant@Dushyants-MacBook-Air.local>
2026-02-28 22:38:43 +0530

    all fixed checking alert

--- backend/app/alert_service.py
+++ backend/app/alert_service.py
@@ -6,38 +6,30 @@ def check_and_create_alert(db, vehicle_id):
 
     health = calculate_health(db, vehicle_id)
 
+    print("HEALTH:", health)
+
     if not health:
+        print("NO HEALTH")
         return
 
-    vehicle = db.query(Vehicle).filter(Vehicle.number_plate == vehicle_id).first()
+    vehicle = db.query(Vehicle).filter(
+        Vehicle.number_plate == vehicle_id
+    ).first()
+
+    print("VEHICLE:", vehicle)
 
     if not vehicle:
+        print("NO VEHICLE")
         return
 
     alerts = []
 
     if health["health_score"] < 50:
+        print("LOW OVERALL")
         alerts.append(("Health Drop", "High", "Overall health below 50%"))
 
     if health["engine_health"] < 40:
+        print("ENGINE ALERT TRIGGERED")
         alerts.append(("Engine Risk", "High", "Engine degradation critical"))
 
-    if health["battery_health"] < 35:
-        alerts.append(("Battery Risk", "Moderate", "Battery health dropping"))
-
-    if health["predicted_life_years"] < 5:
-        alerts.append(("Low Life Remaining", "Critical", "Vehicle near failure"))
-
-    for alert_type, severity, message in alerts:
-        alert = Alert(
-            vehicle_id=vehicle_id,
-            vehicle_name=vehicle.name,
-            vehicle_model=vehicle.model,
-            alert_type=alert_type,
-            severity=severity,
-            message=message,
-            timestamp=datetime.utcnow()
-        )
-        db.add(alert)
-
-    db.commit()
+    print("ALERT LIST:", alerts)
